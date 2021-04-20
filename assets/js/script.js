75ffad4e447a149848e37a39611d288560f8d959
Angela Man <angela.k.man@gmail.com>
2021-04-20 14:33:16 -0500

    basic style for forecast DOM elements and js pseudocode outline

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// collect user city input
+
+// get weather data from one call API weather
+
+// display current weather
+
+// assign corresponding background color to uv index
+
+// assign corresponding weather icon to forecasts
+
+// display 5 day forecast
+
+// save city searches to localStorage
+
+// load city searches from local Storage
