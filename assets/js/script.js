ca461240aa28b1b196b85289d2caddc7201233f0
Angela Man <angela.k.man@gmail.com>
2021-04-21 01:51:05 -0500

    connect user input to fetch API data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 var buttonContainerEl = document.querySelector("btn-container");
 var currentCityEl = document.querySelector("#current-city");
-var currentForecastEl = document.querySelector("forecast-current");
+var currentForecastEl = document.querySelector("#forecast-current");
 var futureForecastEl = document.querySelector("forecast-future");
 
-var searchCity="Houston";
+var searchCity="";
 var searchHistory = [];
 
 // collect user city input
@@ -16,12 +16,15 @@ var formSubmitHandler = function(event) {
     var city = cityInputEl.value.trim();
     searchCity = city;
     console.log(city);
-    //getCoordinates(city);
+    getCoordinates(city);
+
+    // save to searchHistory
+    searchHistory.push(city);
 }
 
 // get longitude and latitude coordinates from city
-var getCoordinates = function() {
-    var geocodeApiUrl = "http://www.mapquestapi.com/geocoding/v1/address?key=4kON7aMII2plUGxg7GnPOJVn3RGuO6qs&location=Washington,DC";
+var getCoordinates = function(searchCity) {
+    var geocodeApiUrl = "http://www.mapquestapi.com/geocoding/v1/address?key=4kON7aMII2plUGxg7GnPOJVn3RGuO6qs&location="+searchCity;
     fetch(geocodeApiUrl).then(function(response) {
         if (response.ok) {
             // if successful, obtain coordinates from response and send to getWeather
@@ -47,6 +50,7 @@ var getWeatherInfo = function(coordindates) {
         if(response.ok) {
             response.json().then(function(data) {
                 displayWeather(data);
+                displayForecast(data);
             })
         }
         else {
@@ -57,12 +61,23 @@ var getWeatherInfo = function(coordindates) {
 
 // display current weather
 var displayWeather = function(weatherData) {
+
+    currentForecastEl.className = "forecast-current";
     
     //today's date
     var date = moment().format("M/DD/YYYY");
 
+    // get current weather icon
+    var weatherIconId = weatherData.current.weather[0].icon;
+    var weatherIconDescription = weatherData.current.weather[0].description;
+    var iconUrl = "http://openweathermap.org/img/wn/"+ weatherIconId +"@2x.png";
+    var weatherIconEl = document.createElement("img");
+    weatherIconEl.setAttribute("src", iconUrl);
+    weatherIconEl.setAttribute("alt", weatherIconDescription);
+
     // display city name
-    currentCityEl.textContent = searchCity + " " + "("+ date +")";
+    currentCityEl.textContent = searchCity + " " + "("+ date +") ";
+    currentCityEl.appendChild(weatherIconEl);
 
     // display current weather
     var currentTempEl = document.querySelector("#current-temp");
@@ -79,14 +94,34 @@ var displayWeather = function(weatherData) {
     
     var currentUviEl = document.querySelector("#current-uvi");
     var uvIndex = weatherData.current.uvi;
+    //uvIndexColor(uvIndex);
     currentUviEl.textContent = "UV Index: " + uvIndex;
 
 }
 
 // assign corresponding background color to uv index
-
-// assign corresponding weather icon to forecasts
 var uvIndexColor = function(uvIndex) {
+    var uvIndexEl = document.querySelector("#uvi");
+        uvIndex.className = "uvi";
+
+    // clear previous uv index class
+    uvIndexEl.removeClass("uv-extreme uv-higher uv-high uv-moderate uv-low");
+
+    if (uvIndex > 10) {
+        uvIndexEl.addClass("uv-extreme");
+    }
+    else if (uvIndex > 7) {
+        uvIndexEl.addClass("uv-higher");
+    }
+    else if(uvIndex > 5) {
+        uvIndexEl.addClass("uv-high");
+    }
+    else if (uvIndex >2) {
+        uvIndexEl.addClass("uv-moderate");
+    }
+    else {
+        uvIndexEl.addClass("uv-low");
+    }
 }
 
 // display 5 day forecast
